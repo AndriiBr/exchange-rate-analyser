d03e5af5a7847bc6e226d04b8bf3b93550cf9e05
Andrii Brazhnyk <kamineko911@gmail.com>
2022-11-24 12:45:21 +0200

    Initial commit

---
+++ src/main/java/com/exchange/rate/analyser/ExchangeRateAnalyserApplication.java
@@ -0,0 +1,13 @@
+package com.exchange.rate.analyser;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExchangeRateAnalyserApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ExchangeRateAnalyserApplication.class, args);
+    }
+
+}
